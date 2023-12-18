1d756448c7b5680535d3e882d75303d3eaf5fd53
Olapoju Kenny Agbomeji <pojuagbomeji@gmail.com>
2023-12-18 11:16:17 +0100

    updated guest component

--- client/src/components/GuestComp/GuestComp.jsx
+++ client/src/components/GuestComp/GuestComp.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const GuestComp = () => {
-    console.log('Rendering GuestComponent');
+
     return (
         <div>
             <h2>Welcome, Guest!</h2>
-            <p>This is some content visible to guests.</p>
+            <p>This is some content visible to guests.!</p>
         </div>
     );
 };
